86db29502053315af4718a3e133e7d71a11e9961
Mike <turner.mike@gmail.com>
2019-06-18 12:04:48 -0400

    created custom packages

---
+++ lion-lib-test/index.js
@@ -0,0 +1,5 @@
+const Lion = require('lion-lib-416');
+const addResult = Lion.add(5, 3);
+const multiplyResult = Lion.multiply(2, 8);
+console.log('addResult', addResult);
+console.log('multiplyResult', multiplyResult);
