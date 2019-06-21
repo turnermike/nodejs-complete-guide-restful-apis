264b39ef1a92dd235c3981951258a37362c2eb90
Mike <turner.mike@gmail.com>
2019-06-21 11:50:30 -0400

    starging on add new course route db update

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const Joi = require('joi');
+// const Joi = require('joi');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const config = require('config');
-const debug = require('debug')('app:startup');
+const debug = require('debug')('app:startup');      // requires env var: export DEBUG=app:startup,app:db
 const logger = require('./middleware/logger');      // middleware
 const courses = require('./routes/courses');        // router
 const home = require('./routes/home');              // router
@@ -20,9 +20,12 @@ app.use(helmet());                                  // helps secure http headers
 app.use('/api/courses', courses);                   // use the courses router object for any routes starting with /api/courses
 app.use('/', home);                                 // use home router object for any routes starting with /
 
+// output app info
 console.log(`Application Name: ${config.get('name')}`);
 console.log(`Mail Host: ${config.get('mail.host')}`);
 console.log(`Mail Password: ${config.get('mail.password')}`);
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+console.log('app.get("env"):', app.get('env'));
 
 // environment specific middleware
 if (app.get('env') === 'development'){
@@ -31,8 +34,8 @@ if (app.get('env') === 'development'){
     app.use(logger);                                    // example of a custom middleware
 }
 
-console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
-console.log(`app: ${app.get('env')}`);
+
+
 
 // start server
 app.listen(port, () => { console.log(`Listening on port ${port}`) });
--- express-demo/routes/courses.js
+++ express-demo/routes/courses.js
@@ -6,18 +6,82 @@
  */
 
 const express = require('express');
+const mongoose = require('mongoose');
+const debug = require('debug')('app:db');
+const morgan = require('morgan');
+const Joi = require('joi');
 const router = express.Router();
 
-// test data
-const courses = [
-    { id: 1, name: 'Course1' },
-    { id: 2, name: 'Course2' },
-    { id: 3, name: 'Course3' }
-];
+// // test data
+// const courses = [
+//     { id: 1, name: 'Course1' },
+//     { id: 2, name: 'Course2' },
+//     { id: 3, name: 'Course3' }
+// ];
+
+// connect to mongodb
+mongoose.connect('mongodb://localhost/node-restful-api', { useNewUrlParser: true, useFindAndModify: false })
+    .then( () => debug('Connected to MongoDB'))
+    .catch(err => debug('Error: ', err));
+
+// initialize courses collection schema
+const courseSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: [2, 'Name must have at least 2 characters'],
+        maxlength: [255, 'Name must have a maximum of 255 characters'],
+        trim: true
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: ['web', 'mobile', 'network'],
+        lowercase: true,
+        trim: true
+    },
+    author: {
+        type: String,
+        required: true,
+        minLength: 4,
+        maxLength: 255
+
+    },
+    tags: {
+        type: Array,
+        validate: {
+            validator: function(v) {
+                return v && v.length > 0;
+            },
+            message: 'A course should have at least one tag.'
+        }
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    price: {
+        type: Number,
+        require: true,
+        // required: function() {
+        //     return this.isPublished;
+        // },
+        min: [5, 'Enter a minimum price of $5'],
+        max: [300, 'Enter a maximum price of $300']
+    }
+
+});
+const Course = mongoose.model('Course', courseSchema);
+
+/**
+ * Routes
+ */
 
 // get all courses
 router.get('/', (req, res) => {
+
     res.send(courses);
+
 });
 
 // get course by id
@@ -39,16 +103,62 @@ router.post('/', (req, res) => {
 
     if ( error ) return res.status(400).send(error.details[0].message);
 
-    // new course data
-    const course = {
-        id: courses.length + 1,
-        name: req.query.name
-    };
+    // trim any spaces after the comma in comma seperated tags
+    let tags = [];
+    req.query.tags.split(',').map(el => { tags.push(el.trim()); });
+    // console.log('tags', tags);
 
-    courses.push(course);
-    console.log('courses', courses);
+    const course = new Course({
+        name: req.query.name,
+        category: req.query.category,
+        author: req.query.author,
+        tags,
+        price: req.query.price
+    });
 
-    res.send(course);
+
+
+    // try{
+
+        const result = course.save();   // .save() returns a promise
+
+        result
+            .then(result => {
+                debug('New course added: \n', result);
+                res.send(result);
+            })
+            .catch(err => {
+                console.log('error', err.errors);
+                res.send(err.errors);
+            });
+
+        // res.send({});
+
+    // }
+    // catch(ex) {
+    //     let returnMessage;
+    //     for(field in ex.errors) {
+    //         console.log('Error field: ', ex.errors[field].message);
+    //         returnMessage += ex.errors[field].message + '\n';
+    //     }
+    //     res.send(ex.errors);
+    // }
+
+
+    // const { error } = validateCourse(req.body);
+
+    // if ( error ) return res.status(400).send(error.details[0].message);
+
+    // // new course data
+    // const course = {
+    //     id: courses.length + 1,
+    //     name: req.query.name
+    // };
+
+    // courses.push(course);
+    // console.log('courses', courses);
+
+    // res.send(course);
 
 });
 
