1a39c0b9331c642b4944e753f36954bd763ad80d
Mike <turner.mike@gmail.com>
2019-06-24 15:29:44 -0400

    moved population demo to mongodb-demo folder

---
+++ mongodb-demo/population.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true })
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
+
+const Authors = mongoose.model('Authors', new mongoose.Schema({
+  name: String,
+  bio: String,
+  website: String
+}));
+
+const Course = mongoose.model('Course', new mongoose.Schema({
+  name: String,
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Authors'
+  }
+}));
+
+async function createAuthor(name, bio, website) {
+  const author = new Author({
+    name,
+    bio,
+    website
+  });
+
+  const result = await author.save();
+  console.log(result);
+}
+
+async function createCourse(name, author) {
+  const course = new Course({
+    name,
+    author
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+async function listCourses() {
+  const courses = await Course
+    .find()
+    .populate('author', 'name -_id')
+    .sort('name')
+    .select('name author');
+  console.log(courses);
+}
+
+// createAuthor('Mike', 'Mikes Bio', 'Mike Website');
+
+// createCourse('C# Course', '5d11187fd6f30009ca6d2d19')
+
+listCourses();
